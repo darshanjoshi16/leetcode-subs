a898fac8b0ff4adf453b6dc3642190277fcafa41
Darshan Joshi <70624714+darshanjoshi16@users.noreply.github.com>
2022-04-03 10:31:32 +0530

    Create Solution.cpp

---
+++ Easy/145/Solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+    void postOrder(TreeNode* node,vector<int> &ans){
+        if(node == NULL){
+            return;
+        }
+        postOrder(node->left,ans);
+        postOrder(node->right,ans);
+        ans.push_back(node->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL) return ans;
+        postOrder(root,ans);
+        return ans;
+    }
+};
