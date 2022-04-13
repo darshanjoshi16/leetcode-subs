8e96331b6196917f79994a7b4b356e23552d79ed
Darshan Joshi <70624714+darshanjoshi16@users.noreply.github.com>
2022-04-13 10:34:12 +0530

    Create Solution.cpp

---
+++ Easy/1346/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        
+        vector<int>::iterator it = arr.begin();
+        for(int i = 0; i < arr.size(); i++){
+            it++;
+            if(arr[i] >= 0 && binary_search(it, arr.end(), arr[i]*2))
+                return true;
+            else if(arr[i]<0 && binary_search(arr.begin(), it, arr[i]*2))
+                return true;
+        }
+        return false;
+    }
+};
