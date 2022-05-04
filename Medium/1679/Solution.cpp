7527d099e7612f4d83768883459366107b4803a1
Darshan Joshi <70624714+darshanjoshi16@users.noreply.github.com>
2022-05-04 10:22:00 +0530

    Create Solution.cpp

---
+++ Medium/1679/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public: // Time/Space: O(nlogn); O(1)
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int count = 0;
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right && nums[left] < k) {  // pruning:  nums[left] < k
+            if (nums[left] + nums[right] < k) {
+                left++;
+            } else if (nums[left] + nums[right] > k) {
+                right--;
+            } else {
+                left++;
+                right--;
+                count++;
+            }
+        }
+        return count;
+    }
+};
