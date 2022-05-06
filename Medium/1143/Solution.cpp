7506f5b9fd05310a9e8aaf7fae23b1cc25405cec
Darshan Joshi <70624714+darshanjoshi16@users.noreply.github.com>
2022-05-06 16:10:33 +0530

    Create Solution.cpp

---
+++ Medium/1143/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+   int lcsDp(string &x , string&y, int m , int n){
+        int dp[m+1][n+1];
+        //Initialization
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0||j==0)dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(x[i-1] == y[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1]; 
+                }else{
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+    int longestCommonSubsequence(string text1, string text2) {
+         return lcsDp(text1,text2,text1.size(),text2.size());       
+    }
+};
